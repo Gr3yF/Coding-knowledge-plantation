534f50586596ccd230d89cdf31b6f54972610e61
Gr3F <nigelfluegel@gmail.com>
2024-11-20 22:51:25 -0500

    Adding to the pandas notes

--- python/pandas_notes.py
+++ python/pandas_notes.py
@@ -45,6 +45,8 @@ print(df)
 #For a non-comma separated file, 
 # supply the separator of the the data in the "sep" arg.
 
+# --------------------------------------------------------------------
+
 #Accessing Data in DataFrames
 accounts = df
 
@@ -63,6 +65,8 @@ print(accounts[['Bank Code', 'Account#']])
 print(accounts[0:2])
 #method 2: list of booleans
 print(accounts[[True, False, True]])
+
+# --------------------------------------------------------------------
 
 #loc and iloc functions (recommended especially for large datasets)
 # loc : access by name
@@ -92,5 +96,178 @@ print(accounts)
 
 #TIP: place a colon in the column or row argument to select all
 
+# --------------------------------------------------------------------
 
+#AGGREGATING AND SUMMARIZING
 
+#DataFrame Methods
+# .count()
+# .min()
+# .max()
+# .first()
+# .last()
+# .sum()
+# .prod() #product 
+# .mean()
+# .median()
+# .std()
+# .var() #variance
+
+#All methods can be run on rows or colums
+#Rows - default, axis=0, axis='rows'
+#Columns - axis=1, axis='columns'
+#To use on a single column or row use the loc and iloc functions
+
+# --------------------------------------------------------------------
+
+#ADDING AND REMOVING COLUMNS
+
+#Adding
+data = {'DATE':['1929-01-01','1930-01-01','1931-01-01','1932-01-01'],
+        'PCDGA':[9.829,7.661,5.911,3.959],
+        }
+pce = pd.DataFrame(data)
+pce.set_index('DATE',inplace=True)
+print(pce)
+#method 1: Via a list
+pce['PCND'] = [33.941, 30.503,25.798000000000002,20.169]
+print(pce)
+#method 2: Via a Series DataFrame
+pcesv = pd.DataFrame({'DATE':['1929-01-01','1930-01-01','1931-01-01','1932-01-01'],
+                      'PCESV':[33.613,31.972,28.963,24.587]})
+pcesv.set_index('DATE', inplace=True)
+pce['PCESV'] = pcesv
+print(pce)
+#method 3: Via other columns
+pce['PCE'] = pce['PCDGA'] + pce['PCND'] + pce['PCESV']
+print(pce)
+#method 4: Via a CSV or data file using the .read_** methods
+
+#Removing
+pce.drop(columns=['PCDGA', 'PCND', 'PCESV'],
+         axis=1,
+         inplace=True) #inplace arg. changes current DF instead of prdoucing a new one
+print(pce)
+
+# --------------------------------------------------------------------
+
+#ADDING AND REMOVING ROWS
+
+#Adding
+#method 1: concat method (append is depreciated)
+data = {'PCE':[45.945]}
+new_row = pd.DataFrame(data,index=['1933-01-01'])
+print(new_row)
+pce = pd.concat([pce, new_row],) #add more DFs to append multiple
+print(pce)
+
+#Removing
+#drop method, axis=0 or default
+pce.drop(['1929-01-01',
+          '1930-01-01'],
+        inplace=True)
+print(pce)
+
+# --------------------------------------------------------------------
+
+#OPERATIONS ON DATAFRAMES
+#you can multiply an entire DataFrame by a value
+#ec = 0.88
+#pce * ec
+#print(pce)
+#you can use the map function to create a new column
+def convert_to_euro(x):
+    return x * 0.88
+
+pce['EURO'] = pce['PCE'].map(convert_to_euro)
+print(pce)
+#map - elements ina column (series)
+#apply - Across rows or columns
+data = {'GCE':[9.622,10.273,10.169,8.946],
+        'GPDI':[17.170,11.428,6.549,1.819],
+        'NE':[0.383,0.323,0.001,0.043],
+        'PCE':[77.383,70.136,60.672,48.715]}
+gdp = pd.DataFrame(data, index=['1929-01-01',
+                                '1930-01-01',
+                                '1931-01-01',
+                                '1932-01-01'])
+print(gdp)
+#Calculate gdp by summing up the rows using the apply method
+import numpy as np
+gdp['GDP'] = gdp.apply(np.sum, axis=1)
+print(gdp)
+
+# --------------------------------------------------------------------
+
+#HEAD method
+# df.head() shows the first 5 rows of the DataFrame
+# We can specify the # of rows to show by entering a integer
+# df.head(3) for the first 3 rows
+
+#TAIL method
+# df.tail() shows the last 5 rows of the DataFrame
+#df.tail(3) shows the last 3 rows
+
+#DESCRIBE method
+# df.describe()
+# outputs a statistical summary of the DataFrame
+# By default it will return summary statistics on all numerical columns
+# Count of the rows, the mean value across the rows, std, the min. value, percentiles (25%, 50%, 75%), and the max value
+# describe takes 3 optional arguments:
+# 1. include= specify the cols. you wish to be included in the statistics or 'all' for all cols or the dtypes you wish to include (i.e. ['float', 'object'])
+# 2. exclude= works in the opposite way of include
+# 3. percentiles= takes a list of the percentiles you wish to see (i.e. [.1, .5, .9] for the 10%,50%,90%)
+
+# --------------------------------------------------------------------
+
+#FILTERING DATA
+# 
+# Column Comparison
+# prices is a DF
+# Ex 1:
+# prices.high > 2160 or prices['high'] > 2160
+# The output is a sequence of boolean values
+# True for whose value is true for the comparison, and false for the rest
+# Ex 2:
+# prices.Symbol == 'AAPL' or prices['Symbol'] == 'AAPL'
+# Again outputs a sequence of boolean values based on the comparison
+# Ex 3:
+# mask_symbol = prices.Symbol == 'AAPL'
+# aapl = prices.loc[mask_symbol]
+# Placing the boolean values into the loc method will return a DataFrame with only the rows that reported True based on the comparison
+
+#Pandas Boolean Operators
+# And = &
+# Or = |
+# Not = ~
+
+#Combining Comparison Conditions
+# Ex:
+# mask_prices = prices['Symbol'] != 'AMZN'
+# mask_date = historical_highs['Date'] > datetime(2020, 4, 1)
+# mask_amzn = mask_prices & mask_date
+# prices.loc[mask_amzn]
+# Resulting DataFrame has only rows that match both conditions\
+
+# --------------------------------------------------------------------
+
+#PLOTTING DATA
+# pandas DataFrames can plot to matplotlib using the .plot() method
+# simply supply the columns to use for the x and y axises as arguments
+# df.plot(x= ,y= )
+# plot method offers a lot of argument options
+# leaving out the x argument defaults to the index
+# 
+# default plot type is the 'line' plot other plot types include:
+# bar
+# barh
+# hist
+# box
+# kde - kernel density estimation
+# density
+# area
+# pie
+# scatter
+# hexbin
+# (Use the kind='line' arg. to change the type of plot)
+#  
