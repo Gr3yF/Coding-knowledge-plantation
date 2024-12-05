5e81c8559a6ce7ef512b58ba0794fa10bc710235
Gr3F <nigelfluegel@gmail.com>
2024-12-04 21:22:36 -0500

    updates to python API notes

--- python/API_notes.py
+++ python/API_notes.py
@@ -135,6 +135,7 @@ request = response.request
 print(request.headers['content-type'])
 
 #Error Handling:
+# HTTP errors (4xx, 5xx, status codes)
 # 4XX status code - Client Errors
 # indicates the request from the client could not be handled properly
 # common causes - bad requests, authentication failures, wrong header, etc.
@@ -155,7 +156,7 @@ print(request.headers['content-type'])
 # 502 Bad Gateway - The API server could not successfully reach another server it needed to cmoplete the response
 # 504 Gateway Timeout - The server (which acts as a gateway) did not get a response from the upstream server in time
 #
-# Handling API errors
+# Handling API errors (HTTP errors)
 import requests
 
 url = 'http://api.music-catalog.com/albums'
@@ -166,7 +167,7 @@ if r.status_code >= 400:
 else:
     print('all good')
 #
-# Handling Connection errors
+# Handling Connection errors (occur before reaching the server)
 import requests
 from requests.exceptions import ConnectionError
 url=''
@@ -198,4 +199,125 @@ except ConnectionError as conn_err:
 # 4: Catch error responses from the API server
 except HTTPError as http_err:
     print(f'HTTP error occurred: {http_err}')
- 
+
+# FastAPI:
+# Fast way to build high-performance APIs using Python
+# key features - 
+# Fast (Very high performance, one of the fastest Python frameworks available)
+# "Low code" and easy to learn (Python annotations and type hints)
+# Robust (Production-ready code with autodoc)
+# Standards-based (Based on OpenAPI and JSON Schema)
+# 
+# Buidling our first web application with FastAPI
+# 1: Install FastAPI (pip install fastapi)
+# 2: Create your app in main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return{"message":"Hello World"}
+# 3: Run the server (fastapi dev main.py)
+#NOTE:
+# - Can't run the FastAPI sever with the "Run this code" button
+# - Define server code in the Python editor as "main.py" instead
+# - Run it from the terminal using the command "fastapi dev main.py"
+# - Verify that the logs in the terminal show "Application startup complete" before testing live server
+# - Once the live server is running, stop the live server by pressing "Control + C" in the same terminal
+# 
+# GET operation
+# GET is the most common HTTP operation
+# When we type a URL into the address bar of our web browser and hit enter, we are sending a GET request
+# Example: https://www.google.com:80/search?q=fastapi
+# The key parts of a GET request are -
+# - Host (www.google.com)
+# - Port (:80 - default)
+# - Path (/search)
+# - Query String (?q=fastapi)
+# tells our browser to search Google for the term "fastapi"
+#
+# Handling a GET request with FastAPI
+# The simplest FastAPI application -
+from fastapi import FastAPI
+
+# Instantiate app
+app = FastAPI()
+
+# Handle get requests to root
+# Which is either the host alone or the host followed by a slash
+@app.get("/")
+def root():
+    return {"message":"Hello World"}
+# The application responds to rquests to 
+# root by sending back a static dictionary 
+# with key "message" and the value "Hello World."
+#
+# Using cURL web client
+# cURL is a convenient script we can use to test our code
+# without a browser.
+# The only requesred argument to cURL is the URL
+# key optional arguments -
+# - verbose (makes the client more talkative)
+# - header (to specify the encoding of POST data)
+# - data (for the data itself)
+#
+# Adding Query Parameters
+# add a new path to our application called "hello"
+# that takes an optional parameter called "name" with
+# the default value "Alan".
+# This endpoint will return a message "Hello Alan" unless
+# there is a name provided in the query string, in which
+# case it will substitute the name parameter.
+# i.e. $ curl http://localhost:8000
+@app.get("/hello")
+def hello(name: str = "Alan"):
+    return {"message": f"Hello {name}"}
+# this URL will change the name to "Steve"
+# http://localhost:8000?name=Steve
+# 
+# POST operations
+# Build endpoints to handle post operations
+# Create a new object
+# Parameters sent via query string as well as request body
+# Can send a lot more info to the server then GET requests
+# Require an application or framework (e.g. cURL, requests)
+#
+# HTTP Request Body
+# Both HTTP rquests and responses can include a message body,
+# which is the data sent after the HTTP header
+# Header specifies the body encoding (defining how to decode)
+# HTTP request bodies support nested data structures
+# JSON and XML are the most common encodings for APIs
+#
+# Python's BaseModel from pydantic library
+# Designed to generate and manage nested model schemas
+# Pydantic - interface to define request and response body schemas
+# Pydantic model schemas consist of a named model with named
+# and typed attributes, which inherit from the pydantic 
+# BaseModel class
+from pydantic import BaseModel
+
+class Review(BaseModel):
+    num_stars: int
+    text: str
+    public: bool = False #default set to false
+
+class MovieReview(BaseModel):
+    movie: str
+    # Nest Review in MovieReview
+    review: Review
+#
+# POST endpoint to create a new movie review -
+# Enpoint - "/reviews"
+# Input - "MoiveReview" (from above)
+# Output - "db_review" (defined elsewhere)
+@app.post("/reviews", response_model=DbReview)
+def create_review(review: MovieReview):
+    # Persist the moview review to the database
+    db_review = crud.create_review(review)
+    #Typically you define a file called crud.py with
+    #custom functions to create, read, update, and delete
+    #objects in the database (example in FastAPI docs).
+    #Return the review including database ID
+    return db_review
